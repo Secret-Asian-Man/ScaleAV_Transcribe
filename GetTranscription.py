164693e350615d9149884d4b2f9675f1fe6d35ac
David Wu <DavidWu18495@gmail.com>
2024-03-20 17:08:00 -0700

    Init commit

---
+++ GetTranscription.py
@@ -0,0 +1,45 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import sys
+import os
+
+video_id = sys.argv[1]
+
+url = "https://www.youtube.com/watch?v={video_id}"
+
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+# Create a new transcript file
+transcript_file = f'{video_id}_transcript.txt'
+open(transcript_file, 'w').close()
+
+# Write the transcript to the file
+timeGaps = []
+prevTime = 0
+for blurb in transcript:
+  if len(blurb['text']) == 1:
+    continue
+
+  timeGap = blurb['start'] - prevTime
+  prevTime = timeStamp = blurb['start']
+
+  hours, remainder = divmod(timeStamp, 3600)
+  minutes, seconds = divmod(remainder, 60)
+
+  timeStampString = f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
+
+  if timeGap > 300: # 5 minutes
+    timeGaps.append(f"{timeStampString}: {round(timeGap)}s")
+  
+  print(f"{timeStampString}: {blurb['text']}")
+  print(f"(Time Gap: {round(timeGap)}s)\n\n")
+
+  with open(f'{video_id}_transcript.txt', 'a') as file:
+    file.write(f"{timeStampString}: {blurb['text']}\n")
+    file.write(f"(Time Gap: {round(timeGap)}s)\n\n")
+
+# Append timeGaps to the beginning of the file
+with open(transcript_file, 'r+') as file:
+  content = file.read()
+  file.seek(0, 0)
+  file.write(f"Large Time Gaps: {timeGaps}\n\n")
+  file.write(content)
---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+
+Installation:
+
+```
+1. source env/bin/activate
+2. pip install -r requirements.txt
+```
